348179b86f432157e89d52015e663c0607507b9c
SeungTaek <tmdxor01@khu.ac.kr>
2025-06-20 23:22:21 +0900

    Add image

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,3 +123,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = "/media/" 
+MEDIA_ROOT = BASE_DIR / "media/"
--- mysite/urls.py
+++ mysite/urls.py
@@ -22,3 +22,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
+
+#added 
+from django.conf import settings 
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls), #a) 
+    path('', include('blog.urls')),
+]
+#added 
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT) 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
